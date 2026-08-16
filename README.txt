99198129c53fb2ae5f01cf509bbe07e078fc67b7
lyvanmanh44h-oss <lyvanmanh44h@gmail.com>
2026-08-16 19:35:05 +0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,2 @@
+THÁC DÊ CAMPING V6 PWA OFFLINE
+Upload index.html, manifest.webmanifest, sw.js, icon-192.png, icon-512.png lên GitHub Pages. V6 thêm Bếp/Lên món và cảnh báo món chưa lên trước thanh toán.
---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME='thac-de-camping-v6';
+const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE_NAME).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
